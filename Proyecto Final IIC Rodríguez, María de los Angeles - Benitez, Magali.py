1354d76d43a4b27e1f47db5baf40bc9c1940cc0f
Magali Benitez <magali.benitez@globant.com>
2022-06-08 22:03:10 -0300

    Definición de Constantes, Variables y una función

---
+++ Proyecto Final IIC Rodríguez, María de los Angeles - Benitez, Magali.py
@@ -0,0 +1,19 @@
+# Declaramos las constantes
+CLAVE= 12345
+DNI= 12345678
+CUENTA_DESTINO =987665
+# Declaramos las variables 
+SaldoPesos = 85000
+SaldoSoles= 3564
+# Declaramos los contadores
+ClaveErronea =0
+DNIErroneo =0
+#*****************************************************
+#Probando una función : no se puede usar antes de definirla, solo despues. Asi que primero hacer todas las funciones antes de cada bloque de código
+print("Ingrese su nombre") #Pr
+nombre = input() 
+
+def saludo(nombre):
+	print("Hola ", nombre, "!")
+saludo(nombre)
+#*****************************************************
